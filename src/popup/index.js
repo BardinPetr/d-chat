7b55d75f06733806d58ca3bdd2f2d8e8c32f0257
losnappas <hanu6@hotmail.com>
2019-06-27 18:07:33 +0300

    remove lib

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './UI/App';
+import App from '../UI/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
