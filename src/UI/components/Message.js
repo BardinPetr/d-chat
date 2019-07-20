45e6a260ac36c17b2a16a047017f4078c58f1333
losnappas <hanu6@hotmail.com>
2019-07-20 15:35:56 +0300

    Remove tooltip

--- src/UI/components/Message.js
+++ src/UI/components/Message.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
 import TimeAgo from 'react-timeago';
-import Tooltip from 'rc-tooltip';
 import 'rc-tooltip/assets/bootstrap.css';
 import Markdown from 'react-markdown';
 import CodeBlock from './CodeBlock';
@@ -9,29 +8,17 @@ import prettyMs from 'pretty-ms';
 import isNumber from 'is-number';
 import { __ } from 'Approot/misc/util';
 
-const timeago = ({timestamp, addr}) => (
-	<span>
-		{new Date(timestamp).toLocaleTimeString()}.
-		<br/>
-		<span>
-			{addr}
-		</span>
-	</span>
-);
-
 const Ping = ({ping}) => (
 	<span className={ping < 500 ? 'ping nice' : ping < 2000 ? 'ping ok' : 'ping bad'}>{prettyMs(ping)}</span>
 );
 
 const Nickname = ({id, addr, refer, timestamp, username, ping}) => (
 	<span>
-		<Tooltip id={id} placement="right" overlay={timeago({timestamp, addr})} mouseEnterDelay={0.3} mouseLeaveDelay={0.5}>
-			<span onClick={() => refer(addr)} className="avatar" data-tip data-for={timestamp} aria-describedby={id}>
-				{username}
-				{' '}
-			</span>
-		</Tooltip>
-		<TimeAgo date={timestamp} minPeriod={5} />
+		<span title={addr} onClick={() => refer(addr)} className="avatar" data-tip data-for={timestamp} aria-describedby={id}>
+			{username}
+			{' '}
+		</span>
+		<TimeAgo title={new Date(timestamp).toLocaleString()} date={timestamp} minPeriod={5} />
 		{ isNumber(ping) && <Ping ping={ping} /> }
 	</span>
 );
@@ -42,7 +29,7 @@ const Message = ({ refer, message, refersToMe, isSubscribed }) => (
 		'refers-to-me': refersToMe,
 	})}>
 		<span>
-			{ isSubscribed && <i className="loader loader-margin loader-dark" title={__('This person still subscribing')} /> }
+			{ !isSubscribed && <i className="loader loader-margin dark" title={__('This person still subscribing')} /> }
 			<Nickname id={message.id} refer={refer} addr={message.addr} username={message.username} timestamp={message.timestamp} ping={message.ping} />
 		</span>
 		<div>
