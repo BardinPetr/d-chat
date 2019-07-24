00f308bec8301e4ec9bef1b2e46ad93b3e8481b4
losnappas <hanu6@hotmail.com>
2019-07-24 19:29:30 +0300

    Fix some bugs

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,7 +1,3 @@
-import './index.css';
 import renderApp from '../UI';
-// import { browserAction } from 'webextension-polyfill';
 
-// Reset badge text on popup open
-// browserAction.setBadgeText({ text: '' });
 renderApp();
