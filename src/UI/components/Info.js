60c48b0db4f110484cbc0cf303fdfbf85e055bc4
losnappas <hanu6@hotmail.com>
2019-08-26 20:44:19 +0300

    Fix iframe width

--- src/UI/components/Info.js
+++ src/UI/components/Info.js
@@ -34,7 +34,7 @@ const Info = () => {
 						'is-hidden': !active
 					})}>
 						{/* Don't load if not expanded */}
-						{ active && <iframe scrolling="no" style={{ width: '100%', height: '600px', }} src="https://nkn-faucet.herokuapp.com" />
+						{ active && <iframe scrolling="no" style={{ width: '400px', height: '600px', }} src="https://nkn-faucet.herokuapp.com" />
 						}
 					</div>
 				</div>
