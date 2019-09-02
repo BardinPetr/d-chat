f1d9aa4cccd4d4ec94cfa5eb3f025115f67aa948
losnappas <hanu6@hotmail.com>
2019-09-02 13:17:02 +0300

    x

--- src/UI/components/TextareaAutoCompleter.js
+++ src/UI/components/TextareaAutoCompleter.js
@@ -43,7 +43,7 @@ const TextareaAutoCompleter = forwardRef(({ source, showingPreview, subscribing,
 		/>
 		{showingPreview && <Markdown
 			source={source}
-			className="is-overlay"
+			className=""
 		/>}
 	</div>
 ));
