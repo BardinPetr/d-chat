0576b8657431efd8062030fc7c3b9b491279f61f
losnappas <hanu6@hotmail.com>
2019-09-22 17:29:57 +0300

    Sleep before logout to allow worker to do work

--- src/UI/containers/Logout.js
+++ src/UI/containers/Logout.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from 'Approot/redux/actions';
 import history from 'Approot/UI/history';
+import sleep from 'sleep-promise';
 
 const Logout = ({ className, children, dispatch }) => (
 	<a className={className} onClick={() => {
-		dispatch(logout()).then(() => history.push('/login'));
+		dispatch(logout()).then(() => sleep(100)).then(() => history.push('/login'));
 	}}>
 		{children}
 	</a>
