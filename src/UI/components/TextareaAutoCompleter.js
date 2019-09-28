63536a0a13f3588c1cc8377d330c67a45bc39b6b
losnappas <hanu6@hotmail.com>
2019-09-28 12:50:30 +0300

    Fix lint on file

--- src/UI/components/TextareaAutoCompleter.js
+++ src/UI/components/TextareaAutoCompleter.js
@@ -14,36 +14,41 @@ const AutofillMentionItem = ({ entity: { char } }) => (
 	<div>{formatAddr(char)}&hellip;</div>
 );
 
-const _outputCaretEnd = (item) => ({ text: item.char, caretPosition: 'end' });
+const _outputCaretEnd = item => ({ text: item.char, caretPosition: 'end' });
 
-const TextareaAutoCompleter = forwardRef(({ source, showingPreview, subs, mention, ...props }, ref) => (
-	<div className="">
-		<Autocomplete
-			className={classnames('textarea', {
-				'is-hidden': showingPreview,
-			})}
-			ref={ref}
-			{...props}
-			trigger={{
-				':': {
-					dataProvider: async token => emoji(token).slice(0, 5),
-					component: AutofillEmojiItem,
-					output: _outputCaretEnd,
-				},
-				// Grab matching subs, grab 5, sort, filter duplicates, map to format.
-				'@': {
-					dataProvider: async token => subs.filter(sub => sub.startsWith(token)).slice(0, 5).sort().filter((i, pos, arr) => !pos || i !== arr[pos-1]).map(sub => ({char: mention(sub) + ' '})),
-					component: AutofillMentionItem,
-					output: _outputCaretEnd,
-				},
-			}}
-			loadingComponent={() => <span className="is-loader" />}
-		/>
-		{showingPreview && <Markdown
-			source={source}
-			className="x-white-space"
-		/>}
-	</div>
-));
+const TextareaAutoCompleter = forwardRef(
+	({ source, showingPreview, subs, mention, ...props }, ref) => (
+		<div className="">
+			<Autocomplete
+				className={classnames('textarea', {
+					'is-hidden': showingPreview,
+				})}
+				ref={ref}
+				{...props}
+				trigger={{
+					':': {
+						dataProvider: async token => emoji(token).slice(0, 5),
+						component: AutofillEmojiItem,
+						output: _outputCaretEnd,
+					},
+					// Grab matching subs, grab 5, sort, filter duplicates, map to format.
+					'@': {
+						dataProvider: async token =>
+							subs
+								.filter(sub => sub.startsWith(token))
+								.slice(0, 5)
+								.sort()
+								.filter((i, pos, arr) => !pos || i !== arr[pos - 1])
+								.map(sub => ({ char: mention(sub) + ' ' })),
+						component: AutofillMentionItem,
+						output: _outputCaretEnd,
+					},
+				}}
+				loadingComponent={() => <span className="is-loader" />}
+			/>
+			{showingPreview && <Markdown source={source} className="x-white-space" />}
+		</div>
+	),
+);
 
 export default TextareaAutoCompleter;
