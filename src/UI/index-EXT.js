7caaeafe9fbc2a96cbcdd89352de72d2cd4e6acd
losnappas <hanu6@hotmail.com>
2019-12-11 17:13:21 +0200

    Remove "await configs" in popup as unneeded

--- src/UI/index-EXT.js
+++ src/UI/index-EXT.js
@@ -1,12 +1,10 @@
 import { Store } from 'webext-redux';
 import App from 'Approot/UI/containers/App';
 import history from 'Approot/UI/history';
-import configs from 'Approot/misc/configs-APP_TARGET';
 
 const store = new Store();
 
-const renderApp = () => store.ready().then(async () => {
-	await configs.$loaded;
+const renderApp = () => store.ready().then(() => {
 	if (window.location.pathname.includes('popup.html')) {
 		if (store.state.navigation.mostRecentPage){
 			history.replace(store.state.navigation.mostRecentPage);
