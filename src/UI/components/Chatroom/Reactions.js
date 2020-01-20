6fa8e830d6c77520d6912e4839d3b3a458af5073
losnappas <hanu6@hotmail.com>
2020-01-20 19:09:38 +0200

    Fix reactions css

--- src/UI/components/Chatroom/Reactions.js
+++ src/UI/components/Chatroom/Reactions.js
@@ -7,6 +7,7 @@ const Reaction = ({ reaction, addReaction }) => (
 		title={reaction._title.slice(0, 100)}
 		className={classnames('button x-has-opacity-1 button', {
 			'is-primary': reaction._haveReacted,
+			'x-reactions-reacted': reaction._haveReacted,
 			'x-is-first-reaction': reaction._count === 0,
 			'is-rounded': reaction._count === 0,
 			'x-is-hover': reaction._count === 0,
@@ -36,6 +37,7 @@ const Reactions = ({ reactions, addReaction, myAddr }) => {
 
 		const haveReacted = reaction.addr === myAddr;
 		const content = reaction.content;
+		const title = `${reaction.username}.${reaction.pubKey?.slice(0, 8)}`;
 
 		const same = acc[content];
 		if (same) {
@@ -45,7 +47,7 @@ const Reactions = ({ reactions, addReaction, myAddr }) => {
 			}
 
 			same._count += 1;
-			same._title += `, ${reaction.username}.${reaction.pubKey?.slice(0, 8)}`;
+			same._title += (same._title == '') ? title : `, ${title}`;
 			same._haveReacted = same._haveReacted || haveReacted;
 			return acc;
 		}
@@ -55,7 +57,7 @@ const Reactions = ({ reactions, addReaction, myAddr }) => {
 			_isAck: isAck(reaction),
 			_haveReacted: haveReacted,
 			_count: 1,
-			_title: `${reaction.username}.${reaction.pubKey?.slice(0, 8)}`
+			_title: title,
 		};
 		return acc;
 	}, {
