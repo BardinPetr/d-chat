e73bd6d4302abb79a4307b4424aeba0435497b7c
losnappas <hanu6@hotmail.com>
2020-03-07 13:18:08 +0200

    Add image expansion, revert font-family

--- src/UI/components/Chatroom/MediaMessage.js
+++ src/UI/components/Chatroom/MediaMessage.js
@@ -1,5 +1,51 @@
 import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
 import { loadAttachment } from 'Approot/database/attachments';
+import { __ } from 'Approot/misc/browser-util-APP_TARGET';
+
+const Attachment = ({ attachment, onLoad }) => {
+	const [expanded, setExpanded] = useState(false);
+	const toggleExpanded = () => setExpanded(expanded => !expanded);
+
+	return (
+		<div className={classnames('x-media-wrapper', {
+			'x-media-expanded': expanded,
+		})}>
+			<a
+				onClick={toggleExpanded}
+				className="button is-link is-small"
+			>
+				{expanded ? __('Contract') : __('Expand')}
+			</a>
+			<div>
+				{(attachment.type.includes('audio') &&
+					<audio
+						className="x-oc-content"
+						controls
+						loop
+						onLoadedData={onLoad}
+						src={attachment.src}
+					/>)
+				|| (attachment.type.includes('image') &&
+					<img
+						className="x-oc-content"
+						src={attachment.src}
+						onLoad={onLoad}
+					/>)
+				|| (attachment.type.includes('video') &&
+					<video
+						className="x-oc-content"
+						controls
+						playsInline
+						loop
+						onLoadedData={onLoad}
+						src={attachment.src}
+					/>)
+				}
+			</div>
+		</div>
+	);
+};
 
 const MediaMessage = ({ content, attachments, stayScrolled }) => {
 	const [attaches, setAttaches] = useState([]);
@@ -27,35 +73,11 @@ const MediaMessage = ({ content, attachments, stayScrolled }) => {
 	}, []);
 
 	return (
-		<div
-			className=""
-		>
+		<div>
 			<div className={'x-media-container is-flex'}>
 				{attaches.map((attach, i) => (
 					<p key={i}>
-						{(attach.type.includes('audio') &&
-							<audio
-								className="x-oc-content"
-								controls
-								loop
-								onLoadedData={stayScrolled}
-								src={attach.src}
-							/>)
-						|| (attach.type.includes('image') &&
-							<img
-								className="x-oc-content"
-								src={attach.src}
-								onLoad={stayScrolled}
-							/>)
-						|| (attach.type.includes('video') &&
-							<video
-								className="x-oc-content"
-								controls
-								playsInline
-								loop
-								onLoadedData={stayScrolled}
-								src={attach.src}
-							/>)}
+						<Attachment attachment={attach} onLoad={stayScrolled} />
 					</p>
 				))}
 			</div>
