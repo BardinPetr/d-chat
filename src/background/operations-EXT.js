59c873191c61b99232fdcd744211b18b9aebe277
losnappas <hanu6@hotmail.com>
2020-05-04 12:20:17 +0300

    Remove popping up on install

--- src/background/operations-EXT.js
+++ src/background/operations-EXT.js
@@ -1,14 +1,3 @@
 export { wrapStore, alias } from 'webext-redux';
-import { runtime } from 'webextension-polyfill';
 
-export const onInstalled = () =>
-	runtime.onInstalled.addListener(async details => {
-		setTimeout(
-			() =>
-				details.reason === 'install' &&
-				browser.tabs.create({
-					url: browser.runtime.getURL('index.html'),
-				}),
-			250,
-		); // Sometimes the register screen would bug out.
-	});
+export const onInstalled = () => {};
