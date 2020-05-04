9a22c8aca264a366339d5894b94f32036d1ba7b1
losnappas <hanu6@hotmail.com>
2020-05-04 11:57:43 +0300

    Increase maxInitialRequests in web-ext context
    I think it's faster..?

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -68,6 +68,7 @@ module.exports = function (webpackEnv) {
 		optimization: {
 			minimize: isEnvProduction && process.env.APP_TARGET === 'WEB',
 			splitChunks: {
+				maxInitialRequests: process.env.APP_TARGET === 'EXT' ? 15 : 4,
 				automaticNameDelimiter: '-',
 				name: false,
 				cacheGroups: {
