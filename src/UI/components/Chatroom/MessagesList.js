ca9a3d15f5ea7aff14df8f7d92c4a7b9cfafe3e4
losnappas <hanu6@hotmail.com>
2020-05-09 13:45:25 +0300

    Remove chat messageList Memo.
    Don't think that did anything.

--- src/UI/components/Chatroom/MessagesList.js
+++ src/UI/components/Chatroom/MessagesList.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useMemo, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import classnames from 'classnames';
 import { __ } from 'Approot/misc/browser-util-APP_TARGET';
 import Reactions from 'Approot/UI/containers/Chatroom/Reactions';
@@ -36,94 +36,90 @@ const MessagesList = ({
 	useEffect(() => {
 		setOriginalLastReadId(lastReadId);
 	}, [topic]);
-	const messagesList = useMemo(() => {
-		const messagesList = [];
+	const messagesList = [];
 
-		// Start a bundle of messages.
-		for (let i = 0; i < messages.length; i++) {
-			let includeHeader = true;
-			let previousMessage;
-			const messagePackKey = messages[i].id || i;
-			const messagesPack = [];
+	// Start a bundle of messages.
+	for (let i = 0; i < messages.length; i++) {
+		let includeHeader = true;
+		let previousMessage;
+		const messagePackKey = messages[i].id || i;
+		const messagesPack = [];
 
-			// Gather the messages that belong to this bundle.
-			while (i < messages.length) {
-				const message = messages[i];
-				const messageIsNotice = isNotice(message);
+		// Gather the messages that belong to this bundle.
+		while (i < messages.length) {
+			const message = messages[i];
+			const messageIsNotice = isNotice(message);
 
-				if (previousMessage) {
-					// Same sender, max n minutes apart.
-					if (
-						!isNotice(previousMessage) &&
+			if (previousMessage) {
+				// Same sender, max n minutes apart.
+				if (
+					!isNotice(previousMessage) &&
 						!messageIsNotice &&
 						previousMessage.addr === message.addr &&
 						new Date(message.timestamp) - new Date(previousMessage.timestamp) <
 							SEPARATE_MESSAGE_TIME
-					) {
-						includeHeader = false;
-					} else {
-						i--;
-						break;
-					}
+				) {
+					includeHeader = false;
+				} else {
+					i--;
+					break;
 				}
+			}
 
-				// Don't want to lose the "last read" indicator after new messages arrive.
-				const lrId = lastReadId || originalLastReadId;
-				if (message.id === lrId) {
-					messagesPack.push(<LastRead key={'lastRead'} />);
-				}
+			// Don't want to lose the "last read" indicator after new messages arrive.
+			const lrId = lastReadId || originalLastReadId;
+			if (message.id === lrId) {
+				messagesPack.push(<LastRead key={'lastRead'} />);
+			}
 
-				const addReaction = msg =>
-					createReaction({
-						...msg,
-						targetID: message.id,
-					});
+			const addReaction = msg =>
+				createReaction({
+					...msg,
+					targetID: message.id,
+				});
 
-				previousMessage = message;
+			previousMessage = message;
 
-				// Check dynamically, otherwise changing accounts makes them go wrong.
-				const isMe = message.addr === myAddr;
-				const refersToMe = !isMe && message.content?.includes(
+			// Check dynamically, otherwise changing accounts makes them go wrong.
+			const isMe = message.addr === myAddr;
+			const refersToMe = !isMe && message.content?.includes(
 					messageIsNotice ? formatAddr(myAddr) : mention(myAddr)
 				);
 				// If message isn't permissioned, it is marked `hidden`, and then we hide it.
-				const isIgnored = mutedUsers.includes(message.addr) || message.hidden;
-				const isSubscribed = isWhisper(message) || subs.includes(message.addr);
+			const isIgnored = mutedUsers.includes(message.addr) || message.hidden;
+			const isSubscribed = isWhisper(message) || subs.includes(message.addr);
 
-				messagesPack.push(
-					<Message
-						className={classnames('is-relative', {
-							'x-me': isMe,
-							'x-refers-to-me': refersToMe,
-						})}
-						includeHeader={includeHeader}
-						refer={refer}
-						message={message}
-						key={message.id}
-						topic={message.topic}
+			messagesPack.push(
+				<Message
+					className={classnames('is-relative', {
+						'x-me': isMe,
+						'x-refers-to-me': refersToMe,
+					})}
+					includeHeader={includeHeader}
+					refer={refer}
+					message={message}
+					key={message.id}
+					topic={message.topic}
+					addReaction={addReaction}
+					subscribed={isSubscribed}
+					ignored={isIgnored}
+				>
+					<Reactions
 						addReaction={addReaction}
-						subscribed={isSubscribed}
-						ignored={isIgnored}
-					>
-						<Reactions
-							addReaction={addReaction}
-							myAddr={myAddr}
-							messageID={message.id}
-							topic={message.topic}
-						/>
-					</Message>
-				);
-				i++;
-			}
-			messagesList.push(
-				<div key={'pack-' + messagePackKey} className="x-message-bundle">
-					{messagesPack}
-				</div>
+						myAddr={myAddr}
+						messageID={message.id}
+						topic={message.topic}
+					/>
+				</Message>
 			);
+			i++;
 		}
-		return messagesList;
-	}, [messages, lastReadId, subs]);
-
+		messagesList.push(
+			<div key={'pack-' + messagePackKey} className="x-message-bundle">
+				{messagesPack}
+			</div>
+		);
+	}
 	return messagesList;
 };
 
