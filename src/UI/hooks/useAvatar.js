98868aa0242f057c199f3997193530961ecfdaf6
losnappas <hanu6@hotmail.com>
2020-05-17 15:37:47 +0300

    Drop avatar cache

--- src/UI/hooks/useAvatar.js
+++ src/UI/hooks/useAvatar.js
@@ -4,16 +4,11 @@
 import { useState, useEffect, useCallback } from 'react';
 import { getContact } from 'Approot/database/contacts';
 
-const cache = {};
-
 const getAvatar = addr => getContact(addr).then(
 	contact => contact?.content?.avatar?.data
 ).then(data => (data instanceof Blob) ? URL.createObjectURL(data) : undefined);
 
 /**
- * Loads avatars into cache from db, so they
- * don't have to be looked up for each message separately.
- *
  * @param string addr NKN addr.
  * @return { avatar, refresh } Avatar image src url, refresh method to reload.
  */
@@ -21,8 +16,7 @@ const useAvatar = (addr) => {
 	const [avatar, setAvatar] = useState(null);
 	const refresh = useCallback(() => {
 		const av = getAvatar(addr);
-		cache[addr] = av;
-		cache[addr].then(setAvatar);
+		av.then(setAvatar);
 	}, [addr]);
 
 	useEffect(() => {
@@ -30,18 +24,15 @@ const useAvatar = (addr) => {
 			return;
 		}
 		let mounted = true;
+		const av = getAvatar(addr);
 
-		if (!cache[addr]) {
-			cache[addr] = getAvatar(addr);
-		}
-		cache[addr].then(av => {
-			if (mounted) {
-				setAvatar(av);
-			}
-		});
+		av.then(a => mounted && setAvatar(a));
 
 		return () => {
 			mounted = false;
+			if (avatar) {
+				URL.revokeObjectURL(avatar);
+			}
 		};
 	}, []);
 
